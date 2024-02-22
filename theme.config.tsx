bbfb8927448e37a1b4cb4288705d32ad425a9dfa
kpietak <kpietak@agh.edu.pl>
2024-02-22 15:58:25 +0100

    chapters 01 & 02 added

---
+++ theme.config.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Programowanie w języku Rust</span>,
+  footer: {
+    text: 'Programowanie w języku Rust',
+  },
+  editLink : {
+    text: ''
+  },
+  feedback: {
+    content: ''
+  }
+}
+
+export default config
