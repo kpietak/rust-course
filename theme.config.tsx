6d32c96c1022e451d651c87b63bb95c6437e7efd
kpietak <kpietak@agh.edu.pl>
2024-03-22 11:37:03 +0100

    [config] link to repo and feedback added

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,15 +3,19 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>Programowanie w języku Rust</span>,
+  project: {
+    link: 'https://github.com/kpietak/rust-course'
+  },
+  docsRepositoryBase: 'https://github.com/kpietak/rust-course/tree/main',
+  editLink : {
+    text: null
+  },
+  feedback: {
+    content: 'Zauważyłeś błąd, masz pomysł co poprawić - zgłoś propozycję!'
+  },
   footer: {
     text: 'Programowanie w języku Rust',
   },
-  editLink : {
-    text: ''
-  },
-  feedback: {
-    content: ''
-  }
 }
 
 export default config
