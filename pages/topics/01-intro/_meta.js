d8761aec0253255ddb3aeb06092b19e4dc5503cc
kpietak <kpietak@agh.edu.pl>
2024-11-22 08:56:05 +0100

    #9 clean-up of _meta.ts files, removed unnecessary component

--- pages/topics/01-intro/_meta.js
+++ pages/topics/01-intro/_meta.js
@@ -1,18 +1,6 @@
-import { type } from "os";
-
 export default {
-  index: {
-    title: "Wprowadzenie",
-    theme: {
-      pagination: true
-    }
-  }, 
-  '02-vars-and-contants': {
-    title: "Zmienne i stałe",
-    theme: {
-      pagination: true
-    }
-  },
+  index: "Wprowadzenie",
+  '02-vars-and-contants': "Zmienne i stałe",
   "03-basic-data-types": "Proste typy danych",
   "04-compound-data-types": "Podstawowe typy złożne",
   "05-functions": "Funkcje",
--- pages/topics/_meta.ts
+++ pages/topics/_meta.ts
@@ -1,47 +1,40 @@
 export default {
     "01-intro": {
-        "title": "Podstawowe konstrukcje języka",
-        "type": "page",
-        // display: "hidden"
+        title: "Podstawowe konstrukcje języka",
+        type: "page"
       },
       "02-ownership": {
-        "title": "Posiadanie",
-        "type": "page",
-        // display: 'hidden'
+        title: "Posiadanie",
+        type: "page"
       },
       "03-structures": {
-        "title": "Struktury danych",
-        "type": "page",
-        // "display": "hidden"
+        title: "Struktury danych",
+        type: "page"
       },
       "04-pattern-matching": {
-        "title": "Wyliczenia i dopasowanie wzorców",
-        "type": "page",
-        // "display": "hidden"
+        title: "Wyliczenia i dopasowanie wzorców",
+        type: "page"
       },
       "05-generics-lifetimes": {
-        "title": "Typy generyczne i czas życia",
-        "type": "page",
-        // "display": "hidden"
+        title: "Typy generyczne i czas życia",
+        type: "page"
       },
       "06-errors": {
-        "title": "Obsługa błędów",
-        "type": "page",
-        // "display": "hidden"
+        title: "Obsługa błędów",
+        type: "page"
       },
       "07-collections": {
-        "title": "Kolekcje i domknięcia",
-        "type": "page",
-        // "display": "hidden"
+        title: "Kolekcje i domknięcia",
+        type: "page"
       },
       "07-modules": {
-        "title": "Moduły",
-        "type": "page",
+        title: "Moduły",
+        type: "page",
         "display": "hidden"
       },
       "08-async": {
-        "title": "Programowanie asynchroniczne",
-        "type": "page",
+        title: "Programowanie asynchroniczne",
+        type: "page",
         "display": "hidden"
       }
       ,
