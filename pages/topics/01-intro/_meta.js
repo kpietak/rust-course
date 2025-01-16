89a15d2188162bd7632477fe1a1727145aa5fc5d
kpietak <kpietak@agh.edu.pl>
2025-01-16 19:54:36 +0100

    #13 new exercises & additional info added to 01-intro

--- pages/topics/01-intro/_meta.js
+++ pages/topics/01-intro/_meta.js
@@ -1,10 +1,26 @@
 export default {
-  index: "Wprowadzenie",
-  '02-vars-and-contants': "Zmienne i stałe",
-  "03-basic-data-types": "Proste typy danych",
-  "04-compound-data-types": "Podstawowe typy złożne",
-  "05-functions": "Funkcje",
-  "06-control-instructions": "Sterowanie przepływem",
-  "07-cargo": "Cargo - tworzenie projektu",
-  "08-exercises": "Ćwiczenia"
+  _: {
+    type: 'separator',
+    title: 'Ćwiczenia'
+  },
+  'index': "Wprowadzenie",
+  '02-vars-and-contants': 'Zmienne i stałe',
+  '03-basic-data-types': 'Proste typy danych',
+  '04-compound-data-types': 'Podstawowe typy złożne',
+  '05-functions': 'Funkcje',
+  '06-control-instructions': 'Sterowanie przepływem',
+  '07-cargo': 'Cargo - tworzenie projektu',
+  __: {
+    type: 'separator',
+    title: 'Zadania'
+  },
+  'ex-01-tictactoe': 'Kółko i krzyżyk',
+  'ex-02-sudoku': 'Sudoku - poprawność planszy',
+  ___: {
+    type: 'separator',
+    title: 'Dodatkowe materiały'
+  },
+  'add-01-cargo': 'Więcej o Cargo',
+  'add-02-unit-tests': 'Testy jednostkowe',
+
 }
