7d373a0c426d6716294297d9b789acde1f7d3a74
Kamil Piętak <kpietak@agh.edu.pl>
2025-01-25 19:51:26 +0100

    Update page.jsx

--- app/[[...mdxPath]]/page.jsx
+++ app/[[...mdxPath]]/page.jsx
@@ -18,8 +18,10 @@ export default async function Page(props) {
   const result = await importPage(params.mdxPath)
   const { default: MDXContent, toc, metadata } = result
   return (
+    <div>
     <Wrapper toc={toc} metadata={metadata}>
       <MDXContent {...props} params={params} />
     </Wrapper>
+    </div>
   )
 }
